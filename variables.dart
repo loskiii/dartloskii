1c26b9d1b9adebb624bf760e9a8624a0fa2c0f9e
loskiii <omoloski5@gmail.com>
2024-10-04 22:16:39 +0300

    Done

---
+++ variables.dart
@@ -0,0 +1,13 @@
+void main() {
+//Varibles
+//<datatype><variable name> = <value>;
+  String name = "Loski";
+  int age = 20;
+  double height = 5.7;
+  bool isStudent = true;
+
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height");
+  print("Is Student: $isStudent");
+}
